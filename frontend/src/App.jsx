e4887ef3247f19c6367e574dcc91c071c328ddb6
Ayush Deb <ayushdeb848@gmail.com>
2025-01-26 23:40:39 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import './App.css'
+import './App.css';
 import PostDetails from './pages/PostDetails';
 import CreatePostForm from './pages/CreatePostForm';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-import { ToastContainer } from "react-toastify";
+import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './context/AuthContext';
-
-
 
 const App = () => {
   return (
     <Router>
-    <AuthProvider>
-      <ToastContainer/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/posts/:id" element={<PostDetails />} />
-      <Route path="/create-post" element={<CreatePostForm />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-    </AuthProvider>
-  </Router>
-  )
+      <AuthProvider>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/create-post" element={<CreatePostForm />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
